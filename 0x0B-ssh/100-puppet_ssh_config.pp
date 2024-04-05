4a27866f1f398c81109f288858612e844e826fb0
MsebeleJnr <msebelegift@gmail.com>
2024-04-05 09:32:34 -0700

    Using puppet

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# using puppet to make changes into our config file
+
+file {'etc/ssh/ssh_config':
+	ensure => present,
+
+content =>"
+	#SSH client configuration
+	host*
+	PasswordAuthentication no
+}
